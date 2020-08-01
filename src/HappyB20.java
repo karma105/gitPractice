a40b1e08e0d0e35106416a1e585921faa9a7cebc
karma105 <karim.mammadov@gmail.com>
2020-08-01 11:56:17 -0400

    update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,7 +1,9 @@
 public class HappyB20 {
     public static void main(String[] args) {
-        System.out.println("B20!");
+        
         System.out.println("Still working with GIT");
+
+        //a brand new line is added
     }
 
 }
